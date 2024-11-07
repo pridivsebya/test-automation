2308944fb3fea5bbcb9bd2b0b5658767d04c071f
Natasha <natalu2793939@mail.ru>
2024-11-07 21:17:52 +0300

    Добавлен тест2Ð

--- src/main/java/playwright/PlaywrightManagement.java
+++ src/main/java/playwright/PlaywrightManagement.java
@@ -3,12 +3,13 @@ package playwright;
 import com.microsoft.playwright.*;
 
 public class PlaywrightManagement {
-    public void setupPlaywright(String url) {
+    public Page setupPlaywright(String url) {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate(url);
             System.out.println(page.title());
+            return page;
         }
     }
 }
--- src/main/test/java/UI/PlaywrightTest.java
+++ src/main/test/java/UI/PlaywrightTest.java
@@ -1,9 +1,13 @@
 package UI;
 
 import com.google.inject.Inject;
+import com.microsoft.playwright.Page;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
+import com.microsoft.playwright.Locator;
 import playwright.PlaywrightManagement;
+
+import javax.swing.*;
 
 @Guice
 public class PlaywrightTest {
@@ -12,7 +16,22 @@ public class PlaywrightTest {
 
     @Test
     public void openPage() {
-        String url = "https://google.com";
+        String url = "https://www.ozon.ru/";
         playwrightManagement.setupPlaywright(url);
     }
-}
+    @Test
+    public void searchPage() {
+        String url = "https://www.ozon.ru/?__rr=1&abt_att=1&origin_referer=www.google.com";
+        Page page = playwrightManagement.setupPlaywright(url);
+
+        page.locator("#stickyHeader > div > div.i3d_9 > div > div > form > div > div.iaa5_33 > input").fill("Книга");
+        page.click("#stickyHeader > div > div.i3d_9 > div > div > form > button > div");
+
+        String pageContent = page.content();
+        if (pageContent.contains("Книги")) {
+            System.out.println("Страница откылась");
+        } else {
+            System.out.println("Страница не открылась :(");
+        }
+    }
+    }

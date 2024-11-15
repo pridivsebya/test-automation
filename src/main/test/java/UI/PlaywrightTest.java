76fdf44c7d81bc714b5d806ee716a22eae814136
Natasha <natalu2793939@mail.ru>
2024-11-15 16:55:00 +0300

    ФДобавлен плагин allure

--- src/main/test/java/UI/PlaywrightTest.java
+++ src/main/test/java/UI/PlaywrightTest.java
@@ -5,28 +5,53 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 import playwright.PlaywrightManagement;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+
 
 @Guice
 public class PlaywrightTest {
 
     @Test
+    @Description("Открытие страницы Google")
     public void openPage() {
         String url = "https://google.ru";
         Page page = PlaywrightManagement.setupPlaywright(url);
     }
 
     @Test
+    @Description("Поиск и открытие страницы в Яндексе")
     public void searchPage() {
         String url = "https://ya.ru/";
-        Page page = PlaywrightManagement.setupPlaywright(url);
+        Page page = openUrl(url);
 
+        searchText(page, "Книга");
+
+        openPage(page);
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        closePlaywright();
+    }
+
+    @Step("Открытие страницы по URL")
+    public Page openUrl(String url) {
+        return PlaywrightManagement.setupPlaywright(url);
+
+    }
+
+    @Step("Заполнение поля ввода текстом")
+    public void searchText(Page page, String value) {
         page.waitForSelector("#text");
-
-        page.locator("#text").fill("Книга");
+        page.locator("#text").fill(value);
         page.keyboard().press("Enter");
 
-        page.waitForSelector("#RelatedBottom", new Page.WaitForSelectorOptions().setTimeout(10000));
+    }
 
+    @Step("Проверка открытия страницы")
+    public void openPage(Page page) {
+        page.waitForSelector("#RelatedBottom", new Page.WaitForSelectorOptions().setTimeout(10000));
         if (page.locator("#RelatedBottom").isVisible()) {
             System.out.println("Страница открылась");
         } else {
@@ -34,8 +59,9 @@ public class PlaywrightTest {
         }
     }
 
-    @AfterSuite
-    public void tearDown() {
+    @Step("Закрытие Playwright")
+    public void closePlaywright() {
         PlaywrightManagement.close();
+        }
     }
-}
+
